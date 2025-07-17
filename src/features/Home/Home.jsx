136c2d251c76231bf0dcf8ab8c0b3826bd3b13a4
MattCollingwood <matt.d.collingwood@gmail.com>
2025-07-17 13:02:50 -0400

    Update Home.jsx

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
 
   useEffect(() => {
     dispatch(fetchPosts(selectedSubreddit));
-  }, [selectedSubreddit]);
+  }, [dispatch]);
 
   const onToggleComments = (index) => {
     const getComments = (permalink) => {
